7d17947c90870d744eaf25763e3afbbe54a0f7a3
ccyjw8860 <ccyjw8860@gmail.com>
2020-12-10 22:13:02 +0900

    checkmate login 기능 만들기 2일차
    #7.2

--- redux/store.js
+++ redux/store.js
@@ -1,8 +1,32 @@
-const { configureStore } = require("@reduxjs/toolkit")
-const { default: rootReducer } = require("./rootReducer")
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from "redux-persist";
+import rootReducer from "./rootReducer";
+import { AsyncStorage } from "react-native";
 
-const store = configureStore({
-    reducer: rootReducer
-})
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+    const store = configureStore({
+        reducer: persistedReducer,
+        middleware: getDefaultMiddleware({
+          serializableCheck: {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+          }
+        })
+      });
+      
+const persistedStore = persistStore(store);
 
 export default store;
